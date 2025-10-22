6eca61ae9f03d8374c7d136cdb6fd7ae5477997b
cse2027 <cseab2027@gmail.com>
2025-10-22 14:33:01 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -13,3 +13,20 @@ data = {
 df = pd.DataFrame(data)
 df.drop_duplicates(inplace=True)
 print("Simulated Unique Dataset:\n", df)
+
+"""
+OUTPUT:
+
+Simulated Unique Dataset:
+    ID  Age  Salary       City
+0   1   29   47904  Hyderabad
+1   2   49   72909       Pune
+2   3   24   46241  Hyderabad
+3   4   35   73002  Hyderabad
+4   5   20   39224       Pune
+5   6   37   39289       Pune
+6   7   47   85552  Bangalore
+7   8   48   61210  Hyderabad
+8   9   45   58712       Pune
+9  10   49   40742  Bangalore
+"""
